0c51d5686c0094dc7929ea4ce364c06a93868b0a
Keisuke Nishida <keisuke.nishida@gmail.com>
2020-12-28 11:14:33 +0900

    Avoid using flow.get_tasks

--- flows/testflow.py
+++ flows/testflow.py
@@ -6,7 +6,6 @@ def get_message():
 
 def test_get_message():
     with Flow('test') as flow:
-        get_message()
+        task1 = get_message()
     state = flow.run()
-    task = flow.get_tasks(name='get_message')[0]
-    assert state.result[task].result == 'Hello, world!'
+    assert state.result[task1].result == 'Hello, world!'
